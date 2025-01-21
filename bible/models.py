541590ce9bb103deb6b6ef71afc2da2c774e2e91
oasisgorilla <yongjae0522@gmail.com>
2025-01-21 16:57:25 +0900

    성경구절 model 작성

--- bible/models.py
+++ bible/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BibleVerse(models.Model):
+    book = models.CharField(max_length=100)
+    chapter = models.IntegerField()
+    verse = models.IntegerField()
+    text = models.TextField()
+
+    class Meta:
+        unique_together = ('book', 'chapter', 'verse')
+
+    def __str__(self):
+        return f"{self.book} {self.chapter}:{self.verse}"
