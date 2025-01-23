1380b1c25a031be2aa9e3dac64100b17fd0225fc
oasisgorilla <yongjae0522@gmail.com>
2025-01-23 18:09:29 +0900

    view.py 최초 작성

--- bible/views.py
+++ bible/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import BibleVerse
+from .serializers import BibleVerseSerializer
+
+class BibleVerseView(APIView):
+    def get(self, request, book, chapter, verse):
+        try:
+            verse = BibleVerse.objects.get(book=book, chapter=chapter, verse=verse)
+            serializer = BibleVerseSerializer(verse)
+            return Response(serializer.data)
+        except BibleVerse.DoesNotExist:
+            return Response({"error": "Verse not found"}, status=404)
