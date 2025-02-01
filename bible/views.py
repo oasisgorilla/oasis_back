5b7d1781b511bec127392f0691cb77bc3596b40a
oasisgorilla <yongjae0522@gmail.com>
2025-02-01 22:34:49 +0900

    book 공백 제거 코드 추가

--- bible/views.py
+++ bible/views.py
@@ -4,10 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import BibleVerse
 from .serializers import BibleVerseSerializer
+import urllib.parse
 
 class BibleVerseView(APIView):
     def get(self, request, book, chapter, verse):
-        book = book.capitalize()
+        book = urllib.parse.unquote(book) # 1%20Samuel 과 같이 오는 요청에서 공백 제거
+        print(book)
         try:
             verse = BibleVerse.objects.get(book=book, chapter=chapter, verse=verse)
             serializer = BibleVerseSerializer(verse)
