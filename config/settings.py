162ddb55b7bd93ae914765f7661886320020b455
oasisgorilla <yongjae0522@gmail.com>
2025-02-13 09:29:29 +0900

    시크릿 키 은닉

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8m)f=4)en0*zub9f%+tz&oyfe$@)%@t96_c0_g@+=y__h7*!)_"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
