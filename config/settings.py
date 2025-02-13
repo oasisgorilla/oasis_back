00dc5ba987feade2d12b7759c3b4a57be6dc135d
oasisgorilla <yongjae0522@gmail.com>
2025-02-13 16:41:11 +0900

    모든 호스트 허용

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # 모든 ip허용(개발용)
 
 
 # Application definition
